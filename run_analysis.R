aff88ccea5888c798662f1b9d2be142001d795ec
James Hoare <jahoare@gmail.com>
2014-12-16 09:34:58 -0500

    Created working run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,14 +1,14 @@
 library(data.table)
-
+library(dplyr)
 ## Import the test data, subject ids and activity IDs
 
 xtest <- read.table("X_test.txt")
 features <- read.table("features.txt")
 names(xtest) <- features[, 2]
 ytest <- read.table("Y_test.txt")
-names(ytest) <- c("Activity_Type")
+names(ytest) <- c("activity_type")
 subtest <- read.table("subject_test.txt")
-names(subtest) <- c("Subject_ID")
+names(subtest) <- c("subject_id")
 test <- cbind(subtest, ytest, xtest)
 
 ## Import the train data, subject ids and activity IDs
@@ -16,9 +16,9 @@ test <- cbind(subtest, ytest, xtest)
 xtrain <- read.table("X_train.txt")
 names(xtrain) <- features[, 2]
 ytrain <- read.table("Y_train.txt")
-names(ytrain) <- c("Activity_Type")
+names(ytrain) <- c("activity_type")
 subtrain <- read.table("subject_train.txt")
-names(subtrain) <- c("Subject_ID")
+names(subtrain) <- c("subject_id")
 train <- cbind(subtrain, ytrain, xtrain)
 
 ## Combine test and train data sets
@@ -27,16 +27,25 @@ wearable_data <- rbind(test, train)
 
 ## Filter to only show mean and std columns
 
-wearable_data <- wearable_data[ , grep("(Subject)|(Activity)|(mean\\(\\))|(std\\(\\))", names(wearable_data))]
+wearable_data <- wearable_data[ , grep("(subject)|(activity)|(mean\\(\\))|(std\\(\\))", names(wearable_data))]
 
 ## Convert Activity_Type to factor
 activity_labels <- read.table("activity_labels.txt")
-wearable_data$Activity_Type <- factor(wearable_data$Activity_Type)
-levels(wearable_data$Activity_Type) <- activity_labels[, 2]
+wearable_data$activity_type <- factor(wearable_data$activity_type)
+levels(wearable_data$activity_type) <- activity_labels[, 2]
 
-## Convert to a data.table
+## Make names appropriate
+names <- make.names(names(wearable_data))
+names <- gsub("(\\.\\.\\.)", "_", names)
+names <- gsub("\\.\\.", "", names)
+names <- gsub("\\.", "_", names)
+names(wearable_data) <- names
 
-wearable_data <- data.table(wearable_data)
+## Use dplyr to group and summarize the mean of each column by groupings 
+
+wearable_data <- tbl_df(wearable_data)
+wearable_data <- group_by(wearable_data, activity_type, subject_id)
+summary <- summarise_each(wearable_data, funs(mean))
 
 ## Clean up unneeded variables
 
@@ -50,7 +59,11 @@ rm(features)
 rm(test)
 rm(train)
 rm(activity_labels)
+rm(names)
+
 
 ## Write the table to a file
-
 write.table(wearable_data, file = "wearable_data.txt", row.names = FALSE)
+
+## Write Summary to a file
+write.table(summary, file = "summary.txt", row.names = FALSE)
